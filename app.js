27ebc449ed6e885161a30796ac3c4d280d0a6956
Arthur Jen <arthurljen@gmail.com>
2018-05-14 17:42:06 -0700

    lots of css and html

---
+++ app.js
@@ -0,0 +1,7 @@
+// const checkbox = getElementbyId('check-0');
+// checkbox.addEventListener('checked', strikethroughItem(), 'true')
+
+// function strikethroughItem(event) {
+
+//     item.style.text-decoration = line-through;
+// }
